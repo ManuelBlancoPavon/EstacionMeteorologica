761d50be006fa9835043d7ef59ca1e91866064d9
manuelblancopavon <manuelblancopavon@gmail.com>
2023-05-15 23:40:11 +0200

    v.2.1 Arreglado script.sql, anadido hoja de estilos, mejorada la pagina web con optimicaciones de codigo y buscador de horas

--- Programacion/Servidor/PaginaWeb/app/main.py
+++ Programacion/Servidor/PaginaWeb/app/main.py
@@ -21,18 +21,32 @@ mydb = mysql.connector.connect(
 # routes
 @app.route('/', methods=['GET', 'POST'])
 def bar():
-    diaDefault = (2023, 4, 27)
-    diaSaneado = 27
-    mesSaneado = 4
-    anoSaneado = 2023
+    diaDefault = (2023, 5, 15)
     diaBuscar = datetime.date(*diaDefault)
+    horaInicio = "00:00:00"
+    horaFin = "23:59:59"
     # diaBuscar = datetime.date.today()
     if request.method == 'POST':
         diaIntroducido = request.form['dia']
         mesIntroducido = request.form['mes']
         anoIntroducido = request.form['ano']
+        horax = request.form['horax']
+        horay = request.form['horay']
+
         if not diaIntroducido or not mesIntroducido or not anoIntroducido:
             return 'Por favor, introduce una fecha.'
+        if horax or horay:
+            try:
+                datetime.datetime.strptime(horax, "%H:%M")
+                horaInicio = horax + ":00"
+            except ValueError:
+                return("Formato de hora inicio incorrecto. Debe ser en formato HH:MM.")
+            try:
+                datetime.datetime.strptime(horay, "%H:%M")
+                horaFin = horay + ":59"
+            except ValueError:
+                return("Formato de hora fin incorrecto. Debe ser en formato HH:MM.")
+        
         try:
             diaOBJ = datetime.datetime.strptime(diaIntroducido, '%d')
             diaSaneado = diaOBJ.strftime('%d')
@@ -48,9 +62,16 @@ def bar():
         except ValueError:
             return 'La fecha introducida no es válida.'
         
-    
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT fecha, temperatura, humedad, presion, precipitaciones, velocidad, direccion, uvi, lux, ppm FROM mediciones")
+    consulta = """
+    SELECT fecha, temperatura, humedad, presion, precipitaciones, velocidad, direccion, uvi, lux, ppm 
+    FROM mediciones 
+    WHERE DATE(fecha) = %s
+    AND TIME(`fecha`) >= %s
+    AND TIME(`fecha`) <= %s
+    ORDER BY fecha DESC
+    """
+    mycursor.execute(consulta, (diaBuscar, horaInicio, horaFin))
     resultado = mycursor.fetchall()
     # Procesamiento de los datos
     fecha = [] # Fecha y hora 9999-99-99 99:99:99
@@ -75,29 +96,23 @@ def bar():
         uv.append(i[7])
         lux.append(i[8])
         ppm.append(i[9])
+    
+    # Quitarle los segundos a la fecha
+    fechaFormateada = []
+    for i in fecha:
+        fechaFormateada.append(i.strftime('%d-%m-%Y %H:%M'))
+    
+    # Quitarle los segundos a horaInicio y horaFin
+    horaInicio = horaInicio[:-3]
+    horaFin = horaFin[:-3]
+    diaBuscarFormateado = diaBuscar.strftime("%d-%m-%Y")
 
-
-    # print(precipitaciones)
-    # Filtrar los datos para incluir solo las fechas correspondientes al día actual
-    fecha_actual = [fecha[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-
-    # Filtrar los datos de temperatura para incluir solo los valores correspondientes al día actual
-    temperatura_actual = [temperatura[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    humedad_actual = [humedad[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    presion_actual = [presion[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    precipitaciones_actual = [precipitaciones[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    velocidad_actual = [velocidad[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    direccion_actual = [direccion[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    uv_actual = [uv[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    lux_actual = [lux[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
-    ppm_actual = [ppm[i] for i in range(len(fecha)) if fecha[i].date() == diaBuscar]
     env = Environment()
     env.globals.update(zip=zip)
-    
-    return render_template("index.html", temperaturas=temperatura_actual,uvs=uv_actual,luxes=lux_actual,ppms=ppm_actual,direcciones=direccion_actual, humedades=humedad_actual, presiones=presion_actual, precipitaciones=precipitaciones_actual, velocidades=velocidad_actual,fechas=fecha_actual, zip=zip)
+    return render_template("index.html", temperaturas=temperatura,uvs=uv,luxes=lux,ppms=ppm,direcciones=direccion, humedades=humedad, presiones=presion, precipitaciones=precipitaciones, velocidades=velocidad,fechas=fechaFormateada,dia=diaBuscarFormateado,horax=horaInicio,horay=horaFin, zip=zip)
 
 @app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='image/favicon.ico')
 if __name__ == "__main__":
-    app.run(port=5000, debug=True, host='192.168.1.38')
+    app.run(port=5000, debug=True, host='192.168.1.40')
